b99c9f3dcdaafa1300d3fcdfedb439ad9ca335b2
Pankaj Sharma <pankaj.sharma@pankajsharmass-493.successivetech.com>
2024-12-04 08:10:24 +0530

    call, apply bind method

--- script.js
+++ script.js
@@ -15,12 +15,87 @@ function sum(a, b) {
   
 const curriedSum = curry(sum);
 
-console.log(curriedSum(1)(2));
 
-
-// polyfill function soution of is Array;
+// Polyfill function soution of is Array;
 Array.myIsArray = function(args){
     return Object.prototype.toString.call(args) === '[object Array]';
 }
 
-console.log("test", Array.myIsArray("123"));
+// polyfill -  map function;
+Array.prototype.myMap = function(callback){
+  let temp = [];
+
+  for(let i=0;i<this.length;i++){
+    temp.push(callback(this[i], i, this));
+  }
+
+  return temp;
+}
+
+// polyfill - filter function;
+
+Array.prototype.myFIlter = function(callback){
+ let temp = [];
+
+ for(let i=0;i<this.length;i++){
+   if(callback(this[i], i, this)){
+     temp.push(this[i]);
+   }
+ }
+ return temp;
+}
+
+// polyfill - reduce function;
+
+Array.prototype.myReduce = function(callback, initialValue){
+    let accumulator = initialValue;
+    for(let i=0;i<this.length;i++){
+      accumulator = accumulator?callback(accumulator, this[i], i, this): this[i];
+    }
+
+    return accumulator;
+}
+
+
+// polyfill - Call method
+Function.prototype.myCall = function(context,...args){
+    if(typeof this !== 'function'){
+     throw new TypeError('myCall() called on a non-function');
+    }
+    context.fn = this;
+    return context.fn(...args);
+}
+
+let obj = {name: 'John'};
+function showName(){
+    console.log(this.name);
+    return this.name;
+}
+
+// polyfill - Apply method
+
+Function.prototype.myApply = function(context,args){
+    if(typeof this !== 'function'){
+     throw new TypeError('myApply() called on a non-function');
+    }
+    
+    if(!Array.isArray(args)){
+      throw new TypeError('myApply() expects an array as the second argument');
+    }
+
+    context.fn = this;
+    return context.fn(...args);
+}
+// polyfill - Bind method
+Function.prototype.myBind = function(context,...args){
+    if(typeof this !== 'function'){
+        throw new TypeError('myApply() called on a non-function');
+       }
+       
+       if(!Array.isArray(args)){
+         throw new TypeError('myApply() expects an array as the second argument');
+       }
+   
+       context.fn = this;
+       return () => context.fn(...args);
+}
